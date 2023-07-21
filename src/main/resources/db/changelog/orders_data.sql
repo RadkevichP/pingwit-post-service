0c7803aa55565105236a50066ab05e49738a5e63
Pavel Radkevich <pradkevich@eisgroup.com>
2023-07-21 08:59:17 +0300

    Добавил в проект скрипты для заполнения таблиц данными

---
+++ src/main/resources/db/changelog/orders_data.sql
@@ -0,0 +1,10 @@
+INSERT INTO public.orders(
+	 date, sender_id, postal_item_type, postal_item_class, price)
+	VALUES (  '2023-07-20', 1, 1, 'first', 6),
+	(  '2023-07-21', 1, 1, 'first', 6),
+	(  '2023-07-21', 1, 2, 'second', 10),
+	(  '2023-07-15', 2, 3, 'third', 15),
+	(  '2023-07-15', 2, 1, 'first', 6),
+	(  '2023-07-17', 3, 3, 'first', 13),
+	(  '2023-07-23', 3, 3, 'second', 8),
+	(  '2023-07-25', 4, 2, 'first', 5);
---
+++ src/main/resources/db/changelog/senders_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.senders(
+	 name, surname, email, phone)
+	VALUES ( 'Alex', 'Terner', 'at@mail.com', '37067894653'),
+	( 'Tina', 'Atina', 'tati@mail.com', '37067893544'),
+	( 'Olga', 'Kirbi', 'ok@mail.com', '37064572395'),
+	( 'Oleg', 'Pirogov', 'oo@mail.com', '37064752053');
